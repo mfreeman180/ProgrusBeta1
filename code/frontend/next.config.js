b113bb1c13804119fecb005e10a0206953375946
mfreeman180 <matt.freeman180@gmail.com>
2025-02-22 21:14:02 -0500

    Update frontend folder with cleaned and updated code

--- code/frontend/next.config.js
+++ code/frontend/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
+  images: { unoptimized: true },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx']
+};
+module.exports = nextConfig;
